b284223b71a9a80c20ccdddbaa8efd1c8f97c994
Marcus Christensson <marcus.christensson@gmail.com>
2025-12-11 22:17:35 +0100

    Add project config

--- src/backend.js
+++ src/backend.js
@@ -1,13 +1,77 @@
+var entities = require('@jetbrains/youtrack-scripting-api/entities');
+
 exports.httpHandler = {
   endpoints: [
     {
+      scope: "project",
       method: 'GET',
-      path: 'debug',
+      path: 'templates',
       handle: function handle(ctx) {
-        // See https://www.jetbrains.com/help/youtrack/devportal-apps/apps-reference-http-handlers.html#request
-        const requestParam = ctx.request.getParameter('test');
-        // See https://www.jetbrains.com/help/youtrack/devportal-apps/apps-reference-http-handlers.html#response
-        ctx.response.json({test: requestParam});
+        const props = ctx.project.extensionProperties;
+        const templates = JSON.parse(props.templates) || [];
+        ctx.response.json({
+          templates: templates,
+        });
+      }
+    },
+    {
+      scope: "project",
+      method: 'POST',
+      path: 'addTemplate',
+      handle: function handle(ctx) {
+        const body = JSON.parse(ctx.request.body);
+        const newTemplate = body.template;
+        if (newTemplate.hasOwnProperty('id') === false || newTemplate.id === '') {
+          ctx.response.status = 400;
+          ctx.response.json({ success: false, message: 'Template must have a valid id.' });
+          return;
+        }
+
+        const articleId = newTemplate?.articleId;
+        if (articleId === undefined || articleId === '') {
+          ctx.response.status = 400;
+          ctx.response.json({ success: false, message: 'Template must have a valid articleId.' });
+          return;
+        }
+
+        const article = entities.Article.findById(articleId);
+        if (article === null) {
+          ctx.response.status = 400;
+          ctx.response.json({ success: false, message: `No article found with articleId ${articleId}.` });
+          return;
+        }
+
+        let props = ctx.project.extensionProperties;
+        let templates = JSON.parse(props.templates) || [];
+        let template = templates.find((t) => t.id === newTemplate.id);
+        if (template) {
+          // Update existing entry.
+          Object.assign(template, newTemplate);
+        } else {
+          // Add new entry.
+          templates.push(newTemplate);
+        }
+        props.templates = JSON.stringify(templates);
+        ctx.response.json({ success: true, templates: templates });
+      }
+    },
+    {
+      scope: "project",
+      method: 'DELETE',
+      path: 'removeTemplate',
+      handle: function handle(ctx) {
+        const body = JSON.parse(ctx.request.body);
+        if (body.hasOwnProperty('id') === false || body.id === '') {
+          ctx.response.status = 400;
+          ctx.response.json({ success: false, message: 'Id missing in request.' });
+          return;
+        }
+        const id = body.id;
+        let props = ctx.project.extensionProperties;
+        const oldTemplates = JSON.parse(props.templates) || [];
+        const updatedTemplates = oldTemplates.filter((t) => t.id !== id);
+        props.templates = JSON.stringify(updatedTemplates);
+        ctx.response.json({ success: true });
       }
     }
   ]
--- src/widgets/templateyt-project-config/app.tsx
+++ src/widgets/templateyt-project-config/app.tsx
@@ -1,19 +1,141 @@
-import React, {memo, useCallback} from 'react';
-import Button from '@jetbrains/ring-ui-built/components/button/button';
-
+import Banner from "@jetbrains/ring-ui-built/components/banner/banner";
+import Button from "@jetbrains/ring-ui-built/components/button/button";
+import { Col, Grid, Row } from "@jetbrains/ring-ui-built/components/grid/grid";
+import Input, { Size } from "@jetbrains/ring-ui-built/components/input/input";
+import List, { ListDataItem } from "@jetbrains/ring-ui-built/components/list/list";
+import React, { memo, useCallback, useEffect, useMemo, useState } from "react";
+import type { Template } from "../../../@types/template";
 // Register widget in YouTrack. To learn more, see https://www.jetbrains.com/help/youtrack/devportal-apps/apps-host-api.html
 const host = await YTApp.register();
 
+const createEmptyTemplate = (): Template => ({
+  id: crypto.randomUUID(),
+  name: "",
+  articleId: "",
+  addOnSave: false,
+  forTicketTypes: [],
+});
+
 const AppComponent: React.FunctionComponent = () => {
-  const callBackend = useCallback(async () => {
-    const result = await host.fetchApp('backend/debug', {query: {test: '123'}});
+  const [templates, setTemplates] = useState<Array<Template>>([]);
+  const [template, setTemplate] = useState<Template>(createEmptyTemplate());
+  const [isDraft, setIsDraft] = useState<boolean>(true);
+  const [failMessage, setFailMessage] = useState<string>("");
+
+  useEffect(() => {
+    host
+      .fetchApp<{ templates: Array<Template> }>("backend/templates", {
+        scope: true,
+        method: "GET",
+      })
+      .then((result) => {
+        // eslint-disable-next-line no-console
+        console.log("Result", result);
+        setTemplates(result.templates);
+      });
+  }, [host]);
+
+  const addOrUpdateTemplate = async (template: Template) => {
+    if (template.name.trim() === "") {
+      console.log("Template name is required");
+      return;
+    }
+    if (template.articleId.trim() === "") {
+      console.log("Article ID is required");
+      return;
+    }
+    const result = await host.fetchApp<{
+      success: Boolean;
+      message?: string;
+      templates?: Array<Template>;
+    }>("backend/addTemplate", {
+      scope: true,
+      method: "POST",
+      body: { template: template },
+    });
     // eslint-disable-next-line no-console
-    console.log('request result', result);
-  }, []);
+    console.log("Add template result", result);
+    if (result.success) {
+      setFailMessage("");
+      setIsDraft(false);
+      setTemplates(result.templates || []);
+    } else {
+      setFailMessage(result.message || "Failed to add or update template.");
+    }
+  };
+
+  const getListItems = (data: Array<Template>): Array<ListDataItem<{ templateItem: Template }>> => {
+    const items: Array<ListDataItem<{ templateItem: Template }>> = data.map(
+      (template: Template) => ({
+        key: template.id,
+        rgItemType: 2,
+        label: template.name,
+        details: `Article ID: ${template.articleId}`,
+        templateItem: template,
+      })
+    );
+    return items;
+  };
+
+  const selectTemplate = (selectedTemplate: Template) => {
+    setTemplate(selectedTemplate);
+    setIsDraft(false);
+    setFailMessage("");
+  };
+
+  const createNewTemplate = () => {
+    setTemplate(createEmptyTemplate());
+    setIsDraft(true);
+    setFailMessage("");
+  };
+
+  const listItems = useMemo(() => getListItems(templates), [templates]);
 
   return (
     <div className="widget">
-      <Button primary onClick={callBackend}>{'Make HTTP Request'}</Button>
+      <Grid className="template-config-panel">
+        <Row>
+          <Col xs={12} sm={4} md={4} lg={4} className="template-list-panel">
+            <List
+              data={listItems}
+              activeIndex={listItems.findIndex((item) => item.templateItem.id === template?.id)}
+              onSelect={(item: ListDataItem<{ templateItem: Template }>) =>
+                selectTemplate(item.templateItem)
+              }
+              activateSingleItem
+            />
+          </Col>
+          <Col xs={12} sm={8} md={8} lg={8}>
+            <div className="template-edit-panel">
+              <Input
+                label="Name"
+                value={template.name}
+                onChange={(e) => setTemplate((prev) => ({ ...prev, name: e.target.value }))}
+                size={Size.L}
+              />
+              <Input
+                label="Article ID"
+                value={template.articleId}
+                onChange={(e) =>
+                  setTemplate((prev) => ({ ...prev, articleId: e.target.value.toUpperCase() }))
+                }
+                size={Size.L}
+              />
+              {failMessage && (
+                <Banner mode="error" title="Failed to store template" withIcon>
+                  {failMessage}
+                </Banner>
+              )}
+              <div className="template-edit-actions">
+                <Button onClick={() => addOrUpdateTemplate(template)}>
+                  {isDraft ? "Add Template" : "Save Template"}
+                </Button>
+                {!isDraft && <Button onClick={() => createNewTemplate()}>Add template</Button>}
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
     </div>
   );
 };
