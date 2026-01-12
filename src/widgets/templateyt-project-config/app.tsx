64a773e03d0714bdaf250b9fae73979f8c8f93b9
Marcus Christensson <marcus.christensson@gmail.com>
2026-01-12 21:12:31 +0100

    Add details to project template list

--- src/widgets/templateyt-project-config/app.tsx
+++ src/widgets/templateyt-project-config/app.tsx
@@ -1,3 +1,4 @@
+import WarningIcon from "@jetbrains/icons/warning-empty";
 import Button from "@jetbrains/ring-ui-built/components/button/button";
 import { Col, Grid, Row } from "@jetbrains/ring-ui-built/components/grid/grid";
 import List, { ListDataItem } from "@jetbrains/ring-ui-built/components/list/list";
@@ -53,16 +54,49 @@ const AppComponent: React.FunctionComponent = () => {
     const articlesMap: Record<string, TemplateArticle> = Object.fromEntries(
       templateArticles.map((article) => [article.articleId, article])
     );
+    const getDetails = (template: Template): [string, boolean] => {
+      if (!template?.validCondition) {
+        return ["Incomplete configuration! No validity condition set.", true];
+      }
+      const validCond = template.validCondition;
+
+      let description = "";
+      if (validCond.when === "entity_is") {
+        description +=
+          validCond.entityType === "issue" ? "Valid when ticket" : "Valid when article";
+      } else if (validCond.when === "field_is") {
+        description += `Valid when ticket field ${validCond.fieldName} is ${validCond.fieldValue}`;
+      } else if (validCond.when === "tag_is") {
+        description += `Valid when tagged with ${validCond.tagName}`;
+      }
+
+      if (template.addCondition === null) {
+        description += ".";
+      } else {
+        const addCond = template.addCondition;
+        description += ", ";
+        if (addCond.when === "field_becomes") {
+          description += `added when ticket field ${addCond.fieldName} becomes ${addCond.fieldValue}`;
+        } else if (addCond.when === "tag_added") {
+          description += `added when tagged with ${addCond.tagName}`;
+        }
+        description += ".";
+      }
+      return [description, false];
+    };
+
     const items: Array<ListDataItem<{ templateItem: Template }>> = data.map(
-      (template: Template) => ({
-        key: template.id,
-        rgItemType: 2,
-        label: template.name,
-        details: `Article: ${template.articleId}: ${
-          articlesMap?.[template.articleId]?.summary || ""
-        }`,
-        templateItem: template,
-      })
+      (template: Template) => {
+        const [details, hasWarning] = getDetails(template);
+        return {
+          key: template.id,
+          rgItemType: 2,
+          label: template.name,
+          details: details,
+          templateItem: template,
+          rightGlyph: hasWarning ? WarningIcon : undefined,
+        };
+      }
     );
     return items;
   };
